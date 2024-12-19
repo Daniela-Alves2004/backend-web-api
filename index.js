0acc56472eac82970f68d0329688cef54d614cd1
Daniela Alves <danieladossantosalves01@gmail.com>
2024-12-19 17:31:09 -0300

    fix: change index.js path

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const libraryRoutes = require('./routes/libraryRoutes');
-const bookRoutes = require('./routes/bookRoutes');
-const errorMiddleware = require('./middlewares/errorMiddleware');
+const authRoutes = require('./src/routes/authRoutes');
+const userRoutes = require('./src/routes/userRoutes');
+const libraryRoutes = require('./src/routes/libraryRoutes');
+const bookRoutes = require('./src/routes/bookRoutes');
+const errorMiddleware = require('./src/middlewares/errorMiddleware');
+const docsRoutes = require('./src/routes/docs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,6 +19,7 @@ app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/libraries', libraryRoutes);
 app.use('/books', bookRoutes);
+app.use('/docs', docsRoutes);
 
 // Middleware de erros
 app.use(errorMiddleware);
